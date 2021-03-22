cabaa7b9a0912cd1dc2bbf6cf5b1c388fb0ec6fc
Sofía Lara <sofilara147@gmail.com>
2021-03-22 18:11:30 -0300

    Primer commit - creación proyecto y paquetes

---
+++ src/main/java/com/lab4/tp1/main/Lab4Tp1Application.java
@@ -0,0 +1,13 @@
+package com.lab4.tp1.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4Tp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4Tp1Application.class, args);
+	}
+
+}
